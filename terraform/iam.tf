e4051c4badee61b45e80f506a3cbe3f07b703998
Benjamin Wu <benjaminwu124@gmail.com>
2018-11-25 22:44:10 -0500

    Add dynamodb:DescribeTable to lambda policy (required for get table)

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -40,7 +40,8 @@ resource "aws_iam_role_policy" "lambda_policy" {
         "dynamodb:PutItem",
         "dynamodb:UpdateItem",
         "dynamodb:DeleteItem",
-        "dynamodb:BatchWriteItem"
+        "dynamodb:BatchWriteItem",
+        "dynamodb:DescribeTable"
       ],
       "Resource": [
         "${aws_dynamodb_table.player_table.arn}",
